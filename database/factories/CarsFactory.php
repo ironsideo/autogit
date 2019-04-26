1c0d07cee23f1ca8831aae3a265ed2e8ae82b00a
io37@njit.edu <Wendy#@1>
2019-04-26 16:29:40 -0400

    Adding the register test

---
+++ database/factories/CarsFactory.php
@@ -0,0 +1,15 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Cars::class, function (Faker $faker) {
+    return [
+        'make'  => $faker->randomElement([ 'ford', 'honda', 'toyota' ]),
+        'model' => $faker->word(),
+        'year'  => $faker->year(),
+        'is_seed' => 1,
+    ];
+});
+
+
+ 
