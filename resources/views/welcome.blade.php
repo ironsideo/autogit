03bce11eb69c68f07e724a88e06a12f8a207a169
io37@njit.edu <Wendy#@1>
2019-04-27 21:40:02 -0400

    Updating my about Us page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -96,6 +96,7 @@
                     <a href="https://nova.laravel.com">Nova</a>
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://youtube.com">YouTube</a>
                 </div>
             </div>
         </div>
